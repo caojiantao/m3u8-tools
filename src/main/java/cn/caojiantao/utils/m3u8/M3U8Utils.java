e4b0145eef7e1d77e829a25b6421f730ba5cab4a
caojiantao <caojiantao30@gmail.com>
2019-10-27 23:39:47 +0800

    调整入参

--- src/main/java/cn/caojiantao/utils/m3u8/M3U8Utils.java
+++ src/main/java/cn/caojiantao/utils/m3u8/M3U8Utils.java
@@ -18,15 +18,17 @@ public class M3U8Utils {
     /**
      * 解析 m3u8 分片数据至本地，并保存新的 index 索引文件
      *
-     * @param urlStr          m3u8 地址
-     * @param name            电影名
-     * @param parent          目标文件夹
-     * @param executorService 任务线程池
+     * @param indexInputStream m3u8 索引文件输入流
+     * @param contextPath      ts 分片上下文路径
+     * @param name             电影名
+     * @param parent           目标文件夹
+     * @param executorService  任务线程池
      */
-    public static void parseM3U8(String urlStr, String name, String parent, ExecutorService executorService) {
-        System.out.println("开始解析 m3u8 视频：" + urlStr);
+    public static void parseM3U8(InputStream indexInputStream, String contextPath, String name, String parent, ExecutorService executorService) {
+        System.out.println("开始解析 m3u8 视频...");
         long time = System.currentTimeMillis();
         try {
+            URL url = new URL(contextPath);
             // 目标文件夹
             File targetDir = new File(parent, name);
             if (!FileUtils.newFile(targetDir, true)) {
@@ -39,22 +41,19 @@ public class M3U8Utils {
                 System.out.println("创建索引文件失败：" + indexFile.getAbsoluteFile());
                 return;
             }
-            URL url = new URL(urlStr);
-            try (InputStream is = url.openStream();
-                 BufferedReader reader = new BufferedReader(new BufferedReader(new InputStreamReader(is)));
+            try (BufferedReader reader = new BufferedReader(new BufferedReader(new InputStreamReader(indexInputStream)));
                  BufferedWriter writer = new BufferedWriter(new FileWriter(indexFile, true))) {
                 String line;
                 int tsIndex = 0;
                 List<ParseTsTask> taskList = new ArrayList<>();
                 while ((line = reader.readLine()) != null) {
-                    if (!line.startsWith("#")) {
+                    if (!line.trim().equals("") && !line.startsWith("#")) {
                         // 分片地址处理
                         if (!line.startsWith("http")) {
                             if (line.startsWith("/")) {
                                 String rootPath = url.getProtocol() + "://" + url.getHost();
                                 line = rootPath + line;
                             } else {
-                                String contextPath = urlStr.substring(0, urlStr.lastIndexOf("/") + 1);
                                 line = contextPath + line;
                             }
                         }
--- src/main/java/cn/caojiantao/utils/m3u8/Main.java
+++ src/main/java/cn/caojiantao/utils/m3u8/Main.java
@@ -1,12 +1,22 @@
 package cn.caojiantao.utils.m3u8;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @author caojiantao
  */
 public class Main {
 
     public static void main(String[] args) {
-        String url = "";
-        M3U8Utils.parseM3U8(url, "测试", "D:/电影资源", null);
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+        try (InputStream is = new FileInputStream("/Users/caojiantao/Desktop/test.m3u8")) {
+            M3U8Utils.parseM3U8(is, "http://data.video.iqiyi.com/videos/vts/20190923/16/01/", "盗梦空间", "/Users/caojiantao", executorService);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
